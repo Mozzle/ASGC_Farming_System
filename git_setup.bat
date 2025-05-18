918eb52e6e4eb41cb481724fcbf35a131122e40d
Dylan <dycollier0@gmail.com>
2025-05-17 20:49:13 -0500

    Create git_setup.bat

---
+++ git_setup.bat
@@ -0,0 +1,9 @@
+git update-index --skip-worktree .\NUCLEO-H755ZI\ASGC_Farming_System\ASGC_Farming_System.ioc
+git update-index --skip-worktree *.project
+git update-index --skip-worktree *.mxproject
+git update-index --skip-worktree .\NUCLEO-H755ZI\CM4\.settings\*
+git update-index --skip-worktree .\NUCLEO-H755ZI\CM7\.settings\*
+git update-index --skip-worktree .\NUCLEO-H755ZI\CM4\Debug\*
+git update-index --skip-worktree .\NUCLEO-H755ZI\CM7\Debug\*
+git update-index --skip-worktree *.cproject
+pause
