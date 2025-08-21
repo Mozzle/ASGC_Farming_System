9b70cae2acc76accde1f63def1cf0c7a80034a96
Dylan <dycollier0@gmail.com>
2025-08-21 13:06:37 -0500

    Fix typo

--- NUCLEO-H755ZI/ASGC_Farming_System/CM7/Core/Src/SEN0169.c
+++ NUCLEO-H755ZI/ASGC_Farming_System/CM7/Core/Src/SEN0169.c
@@ -63,7 +63,7 @@ SYS_RESULT SEN0169_Measure(SEN0169_pH_Data *pH_Data) {
 	SYS_RESULT ret_val = SYS_INVALID;
 
 	/*-------------------------------------------------------------------------
-	If fan interface is switchboard disabled, exit without doing anything.
+	If driver is switchboard disabled, exit without doing anything.
 	-------------------------------------------------------------------------*/
 	if (SEN0169_ENABLED == SYS_FEATURE_DISABLED) {
 		ret_val = SYS_DEVICE_DISABLED;
