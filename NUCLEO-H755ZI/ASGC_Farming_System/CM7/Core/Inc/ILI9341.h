69b81babe9bc60e663ee174a5d62c16965a25b01
V3NTYY <165188660+V3NTYY@users.noreply.github.com>
2025-08-29 14:48:41 -0500

    Create initial ILI9341 Class
    First inital push for ILI9341

---
+++ NUCLEO-H755ZI/ASGC_Farming_System/CM7/Core/Inc/ILI9341.h
@@ -0,0 +1,20 @@
+/*-----------------------------------------------------------------------------
+ *
+ * 	ILI9341.h
+ *
+ *  Created on: 8/29/25 by Ben Kensington
+ *
+ *  DESCRIPTION: ILI9341 Display Driver
+ *
+-----------------------------------------------------------------------------*/
+
+#ifndef ILI9341_H
+#define ILI9341_H
+
+#include "main.h"
+#include "functionality_mngmnt.h"
+#include <stdbool.h>
+
+bool ILI9341_Init(uint32_t timeout);
+
+#endif
